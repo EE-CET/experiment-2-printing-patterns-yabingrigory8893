2962afe0c25d67749a75af7b2fecea687add6930
yabingrigory8893 <yabingrigory3@gmail.com>
2026-04-09 22:43:46 -0700

    Update PatternPrinting.java

--- src/PatternPrinting.java
+++ src/PatternPrinting.java
@@ -1,5 +1,20 @@
 import java.util.Scanner;
-
 public class PatternPrinting {
-    
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                if (i < n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.println("*");
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                System.out.println("*");
+            }
+        }
+        sc.close();
+    }
 }
